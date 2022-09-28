b340de3c32c5d7c9506f5fc418ca8ec289bb1adf
Constancegit status <constanceingram94@gmail.com>
2022-09-28 10:55:00 -0600

    added some basic components and tailwind styling

---
+++ components/header.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <div className="flex flex-row justify-center bg-black w-screen text-white text-center">
+        <h1 className="font-bold text-center">🌙 Der Wunderkammer 🌙</h1>
+    </div>
+  )
+}
